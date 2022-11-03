091869f2b393d1415a29b78346a74d394610adef
Albert <bachkovalbert@mail.ru>
2022-11-03 15:48:09 +0500

    CHANGED: +03

---
+++ 03/1.js
@@ -0,0 +1,2 @@
+const grad = Number.parseInt(prompt('Введите температуру в градусач Цельсия'));
+alert(`Цельсий: ${grad}, Фаренгейт: ${((9 / 5) * grad + 32).toFixed(1)} `)
---
+++ 03/2.js
@@ -0,0 +1,9 @@
+function hello(n) {
+    console.log(`Привет ${n}`);
+}
+
+const nam = prompt('Введите имя:');
+hello(nam);
+
+const man = prompt('Как вас зовут?');
+hello(man);
